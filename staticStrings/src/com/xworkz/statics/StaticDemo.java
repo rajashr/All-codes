e905f467dbeb80c36d262cbf1518e9271f226cd3
rajashr <72985515+rajashr@users.noreply.github.com>
2023-09-30 16:54:44 +0530

    First comit

---
+++ staticStrings/src/com/xworkz/statics/StaticDemo.java
@@ -0,0 +1,24 @@
+package com.xworkz.statics;
+
+public class StaticDemo {
+	public static void main(String[] args) {
+		//static method belongs to class not object
+		//it can access static data but not non-static instance data
+		//non-static memebr belongs to instance
+		//in static context you dont have instance 
+		//thats y  you cannot access the instance member without explicitly
+		//mentioning the object reference
+		//staic block is used to initialise the static variable it is executed 
+		//only once when the class is lfirst loaded
+		//static method can be called without creating object of class by just class name
+		//static method can only call static method and cannot call non static method frm it 
+		//static method cannot refer to this or super in anyway
+		//nested classs can be staatic static variable
+		//we cannot override the static inherited methods because in case of overriding the 
+		//as in java overriding takes place by resolving type of object at runtime
+		//static method belongs to class level it is rrsovled at compile time
+		
+		
+	}
+
+}
